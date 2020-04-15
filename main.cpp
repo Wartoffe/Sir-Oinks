0f27291df74c9889c0d9382cbed4264516c87928
Wartoffe <63714418+Wartoffe@users.noreply.github.com>
2020-04-15 12:35:23 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+
+using namespace std;
+
+class Sir_Oinks
+{
+private:
+
+    double v;
+    bool gr;
+    bool hat;
+    bool clinging;
+    class Pozicija;
+
+public:
+
+
+
+
+};
+
+
+class Level
+{
+private:
+
+    int id;
+    class Scena;
+    class Sir_Oinks;
+    class turf;
+    bool izazov;
+public:
+
+};
+
+class Trofej
+{
+private:
+
+    int points;
+    class Pozicija;
+    bool prisutan;
+
+public:
+
+};
+
+class Turf
+{
+private:
+
+    double maxv;
+    bool bouncy;
+    bool slip;
+    bool sharp;
+    class Pozicija;
+
+public:
+};
+
+class Pozicija
+{
+private:
+
+    int x;
+    int y;
+public:
+};
+
+class Scena
+{
+private:
+    int p;
+    int j;
+    int sc[p][j];
+public:
+
+};
+
+class Prodavac
+{
+private:
+
+    class dijalog;
+    int cena;
+    int itemid;
+
+public:
+};
+
+class Izazov
+{
+private:
+
+
+};
+
+class Korisnik
+{
+private:
+
+    int hs;
+    int time;
+    int levelnum;
+    string ime;
+public:
+
+};
+
+class Dijalog
+{
+private:
+
+    string dijalog;
+    int karma;
+
+public:
+};
+int main()
+{
+
+    return 0;
+}
